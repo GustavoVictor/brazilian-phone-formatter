809ba455402d6a841f51cca8f6be5c92f32f52e0
Gustavo Assunção <gustavovictor94@gmail.com>
2023-11-20 23:14:40 -0300

    Update BrazilianFormatter.cs
    Add missing using

--- BrazilianFormatter.cs
+++ BrazilianFormatter.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
 public class BrazilianFormatter
 {
   public static string Format(string txt)
